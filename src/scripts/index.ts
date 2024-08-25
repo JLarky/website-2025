c148c210ff29fa6232838d307fd5a16bcc884242
JLarky <jlarky@gmail.com>
2024-08-24 22:31:28 -0600

    make two of them

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,15 +1,15 @@
-async function onClick() {
-  const response = await fetch('/api/cat-names');
-  const json = await response.json();
-  const { catNames } = json;
-  const index = Math.floor(Math.random() * catNames.length);
-  const catName = catNames[index];
-  document.body.innerText = catName;
-}
-
 class RevealButton extends HTMLElement {
+  onClick = async () => {
+    const response = await fetch('/api/cat-names');
+    const json = await response.json();
+    const { catNames } = json;
+    const index = Math.floor(Math.random() * catNames.length);
+    const catName = catNames[index];
+    this.innerText = catName;
+  }
+
   connectedCallback() {
-    this.querySelector('button')?.addEventListener('click', onClick);
+    this.querySelector('button')?.addEventListener('click', this.onClick);
   }
 }
 
