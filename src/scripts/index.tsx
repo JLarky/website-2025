eece86461c18720fb66da6a2716a522044d1ef84
Yaroslav Lapin <ylapin@legalzoom.com>
2024-09-01 12:19:50 -0600

    confirm it's compatible with solid

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,28 +1,8 @@
 import { defineElement } from '@jlarky/solid-wc';
 
-import { createSignal, Show } from 'solid-js';
+import RevealButton from '../components/RevealButton';
 
 defineElement('reveal-button', { cats: '' }, ({ element, setRenderRoot }) => {
   setRenderRoot(element.attachShadow({ mode: 'open' }));
-  return (props) => {
-    const [selectedCat, setSelectedCat] = createSignal('');
-    const onClick = () => {
-      const catNames = props.cats.split(',');
-      const index = Math.floor(Math.random() * catNames.length);
-      const catName = catNames[index];
-      if (catName) {
-        setSelectedCat(catName);
-      }
-    };
-    return (
-      <>
-        {selectedCat()}
-        <Show when={!selectedCat()}>
-          <button type="button" onClick={onClick}>
-            Reveal one of the {props.cats?.split(',').length} cat names
-          </button>
-        </Show>
-      </>
-    );
-  };
+  return (props) => <RevealButton cats={props.cats.split(',')} />;
 });
