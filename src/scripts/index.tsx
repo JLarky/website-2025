215cc2703960d70725f43d119ace6142c6f47f5e
Yaroslav Lapin <ylapin@legalzoom.com>
2024-09-01 12:17:39 -0600

    solidify

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,18 +1,28 @@
 import { defineElement } from '@jlarky/solid-wc';
 
+import { createSignal, Show } from 'solid-js';
+
 defineElement('reveal-button', { cats: '' }, ({ element, setRenderRoot }) => {
   setRenderRoot(element.attachShadow({ mode: 'open' }));
   return (props) => {
+    const [selectedCat, setSelectedCat] = createSignal('');
     const onClick = () => {
       const catNames = props.cats.split(',');
       const index = Math.floor(Math.random() * catNames.length);
       const catName = catNames[index];
-      element.innerText = catName ?? '';
+      if (catName) {
+        setSelectedCat(catName);
+      }
     };
     return (
-      <button type="button" onClick={onClick}>
-        Reveal one of the {props.cats?.split(',').length} cat names
-      </button>
+      <>
+        {selectedCat()}
+        <Show when={!selectedCat()}>
+          <button type="button" onClick={onClick}>
+            Reveal one of the {props.cats?.split(',').length} cat names
+          </button>
+        </Show>
+      </>
     );
   };
 });
