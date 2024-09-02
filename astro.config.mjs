44abd4f12c9a0d2db7f9695856f551448f8ecce3
Yaroslav Lapin <ylapin@legalzoom.com>
2024-09-01 21:20:22 -0600

    add sourcemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs()],
+  vite: {
+    build: {
+      sourcemap: true,
+    },
+  },
 });
