a2959f148ef8a162bdee8ecd2e31bff90b24faed
Yaroslav Lapin <ylapin@legalzoom.com>
2024-09-01 21:21:00 -0600

    add lit

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,8 +1,6 @@
 import { defineElement } from '@jlarky/solid-wc';
 
-import RevealButton from '../components/RevealButton';
-
 defineElement('reveal-button', { cats: '' }, ({ element, setRenderRoot }) => {
   setRenderRoot(element.attachShadow({ mode: 'open' }));
-  return (props) => <RevealButton cats={props.cats.split(',')} />;
+  return (props) => <div>Hello from MyElement!</div>;
 });
